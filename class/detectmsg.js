627163b3de838014746e065bc69c05bd9add91f2
huda0209 <huda030209@gmail.com>
2020-08-14 18:23:38 +0900

    [add]機能分割により、自己紹介書き込み検知部分のコードをclass文に移植

---
+++ class/detectmsg.js
@@ -0,0 +1,49 @@
+/*
+
+created by huda0209
+minigamepartybot for discord bot 
+
+main.js :MAIN  'MAIN CODE'
+ -kick.js :CLASS  'kick the member'
+ -ban.js :CLASS  'ban the member'
+ -detectmsg.js :CLASS  'detect to write self introduction and add the role'  <= this
+ -announce_new_member :CLASS  'announce new member'
+ -detect-reaction-rule.js :CLASS  'detect to react the message for our server's rule'
+
+ran by node.js and discord.js
+
+2020-8-14
+
+*/
+
+
+class detectmsgevent {
+    constructor(message,client,fs,letter,json) {
+        this.message = message;
+        this.client = client;
+        this.fs = fs;
+        this.letter = letter;
+        this.json = json
+    }
+
+    async detectmsg (){
+        const message = this.message;
+        const client = this.client;
+        const fs = this.fs;
+        const letter = this.letter;
+        const json = this.json;
+
+
+        if(message.channel.id === json.guild.Channel.SelfIntoroduction && !message.member.roles.cache.get(json.guild.Role.member)){
+            const member = message.member
+            await member.roles.add(json.guild.Role.write)
+            if(member.roles.cache.get(json.guild.Role.write) && member.roles.cache.get(json.guild.Role.check)){
+                await member.roles.remove(json.guild.Role.write)
+                await member.roles.remove(json.guild.Role.check)
+                await member.roles.add(json.guild.Role.member)
+            }
+        }
+    }
+}
+
+module.exports = detectmsgevent
