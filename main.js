b121827a39580d445427466296437d40c42cff77
huda0209 <huda030209@gmail.com>
2020-09-05 22:17:04 +0900

    [fix/add]setting.jsonの定数化を変数に修正 / 起動時のトークン指定機能を追加

--- main.js
+++ main.js
@@ -30,14 +30,14 @@ const anmevent = require('./class/announce_new_member.js')
 const detectteactionruleevent = require('./class/detect-reaction-rule.js')
 
 //other 
-const json = JSON.parse(fs.readFileSync('./setting.json','utf8'));
+var json = JSON.parse(fs.readFileSync('./setting.json','utf8'));
 const client = new discord.Client();
 const letter = [[":zero:","0⃣"],[":one:","1⃣"],[":two:","2⃣"],[":three:","3⃣"],[":four:","4⃣"],[":five:","5⃣"],[":six:","6⃣"],[":seven:","7⃣"],[":eight:","8⃣"],[":nine:","9⃣"],[":keycap_ten:","🔟"],[":regional_indicator_a:","🇦"],[":regional_indicator_b:","🇧"],[":regional_indicator_c:","🇨"],[":regional_indicator_d:","🇩"],[":regional_indicator_e:","🇪"],[":regional_indicator_f:","🇫"],[":regional_indicator_g:","🇬"],[":regional_indicator_h:","🇭"],[":regional_indicator_i:","🇮"],[":regional_indicator_j:","🇯"],[":regional_indicator_k:","🇰"],[":regional_indicator_l:","🇱"],[":regional_indicator_m:","🇲"],[":regional_indicator_n:","🇳"],[":regional_indicator_o:","🇴"],[":regional_indicator_p:","🇵"],[":regional_indicator_q:","🇶"],[":regional_indicator_r:","🇷"],[":regional_indicator_s:","🇸"],[":regional_indicator_t:","🇹"],[":regional_indicator_u:","🇺"],[":regional_indicator_v:","🇻"],[":regional_indicator_w:","🇼"],[":regional_indicator_x:","🇽"],[":regional_indicator_y:","🇾"],[":regional_indicator_z:","🇿"]]
 
 
 //start the bot
 client.on("ready", message => {
-  console.log("bot is ready!");
+  console.log(`bot is ready! login : ${client.user.tag}`);
   client.user.setActivity('MiniGamePartyBot Ver 2.1.1', { type: 'PLAYING' })
   //client.user.setActivity(process.env.ver, { type: 'PLAYING' })
   client.channels.cache.get(json.guild.Channel.Rule).messages.fetch(json.guild.Panel.Rule)
@@ -86,9 +86,28 @@ client.on("messageReactionAdd", async(messageReaction ,user) =>{
 })    
 
 
-if (process.env.DISCORD_BOT_TOKEN == undefined) {
-  console.log("please set ENV: DISCORD_BOT_TOKEN");
+if(process.env.DISCORD_BOT_TOKEN == undefined || process.env.DISCORD_BOT_TOKEN == ""){
+  console.log("please set ENV : DISCORD_BOT_TOKEN");
+  process.exit(0);
+}
+if(process.env.TEST_TOKEN == undefined || process.env.TEST_TOKEN == ""){
+  console.log("please set ENV : TEST_TOKEN");
   process.exit(0);
 }
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+
+var token;
+if(process.argv.length>=3){
+  switch(process.argv[2]){
+    case "main" :
+      token = process.env.DISCORD_BOT_TOKEN;
+      break;
+    case "test" :
+      token = process.env.TEST_TOKEN;
+      break;
+    default :
+      console.log(`\nUnknown command. \nUsage \n node main.js main : main token \n node main.js test : test token`)
+      process.exit(0);
+  };
+}else token = process.env.DISCORD_BOT_TOKEN
+client.login(token);
